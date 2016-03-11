6e4de992b5d23244db3ffaebce945604a45267a6
daisysarge <danielle.dunstan@yahoo.com>
2016-03-11 12:56:01 -0500

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,11 @@
-## CacheMatrix.r
-## Functionality include 2 items to cache, calculate and retrieve/set the inverse of given matrix: 
-## 1 - makeCacheMatrix() 
-## 2 - cacheSolve()
-
-## Caching the Matrix to set and get it's inverse upon command
+## Put comments here that give an overall description of what your
+## functions do
+# CacheMatrix.r
+# Functionality include 2 items to cache, calculate and retrieve/set the inverse of given matrix: 
+# 1 - makeCacheMatrix() 
+# 2 - cacheSolve()
+## Write a short comment describing this function
+# Caching the Matrix to set and get it's inverse upon command after checking for NULL
 makeCacheMatrix <- function(x = matrix()) {
   i<-NULL
   set<- function(y) {
@@ -16,8 +18,12 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get,
        setinverse = setinverse, getinverse = getinverse)
 }
-## Caching to retrieve the cached matrix in order to provide the inverse, then setting it. If the cached inverse is not null, message is presented followed by the casched inverse.
+
+
+## Write a short comment describing this function
+# Caching to retrieve the cached matrix in order to provide the inverse, then setting it. If the cached inverse is not null, message is presented followed by the cached inverse.
 cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
   i <- x$getinverse()
   if(!is.null(i)) {
     message("getting cached data")
@@ -26,17 +32,16 @@ cacheSolve <- function(x, ...) {
   data <- x$get()
   i <- solve(data, ...)
   x$setinverse(i)
-  i
+  i      
 }
-## Testing the code
-## This creates and displays the Matrix
+# Testing the code
+# This creates and displays the Matrix
 x<-matrix(1:4,nrow=2,ncol=2)
 print(x)
-## This creates a Cached Matrix and displays the environments
+# This creates a Cached Matrix and displays the environments
 xx<-makeCacheMatrix(x)
 print(xx)
-## This creates the Inverse from the Cache
+# This creates the Inverse from the Cache
 cacheSolve (xx)
-## This retrieves the already generated Cache Inverse
+# This retrieves the already generated Cache Inverse
 cacheSolve (xx)
-
